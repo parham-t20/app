44ff6c7f9359e7c67ee8b6993884da665dd15102
parham-t20 <parhamoman2010@gmail.com>
2025-06-27 21:08:05 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 from datetime import datetime
 from zoneinfo import ZoneInfo
+from functools import partial
 
-# دیکشنری برخی کشورها و منطقه‌های زمانی‌شان
+# دیکشنری کشورها و منطقه زمانی
 country_timezones = {
     "iran": "Asia/Tehran",
     "usa": "America/New_York",
@@ -22,10 +24,114 @@ country_timezones = {
     "uk": "Europe/London"
 }
 
-class TimeZone(App):
-    def build(self):
-        clock = ""
-        name_cont = ""
+
+class MainScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        box = BoxLayout(orientation='vertical', size_hint=(1, None), spacing=20, padding=10)
+
+        btn_connect = Button(text='Connect To Device', size_hint_y=None, height=80)
+        btn_time_zone = Button(text="Time Zone Finder", size_hint_y=None, height=80)
+
+        btn_connect.bind(on_press=partial(self.change_screen, 'device'))
+        btn_time_zone.bind(on_press=partial(self.change_screen, 'timezone'))
+
+        box.add_widget(btn_connect)
+        box.add_widget(btn_time_zone)
+        box.bind(minimum_height=box.setter('height'))
+
+        anchor = AnchorLayout(anchor_y='top')
+        anchor.add_widget(box)
+        self.add_widget(anchor)
+
+    def change_screen(self, screen_name, instance):
+        self.manager.current = screen_name
+
+
+class DeviceScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        box1 = BoxLayout(orientation='vertical', size_hint=(1, None), spacing=22, padding=[10, 30, 10, 10])
+
+        self.lbl = Label(text='IP Address', font_size=30)
+        self.txt_input_ip = TextInput(text='192.168.', multiline=False, size_hint_y=None, height=50, font_size=30)
+        btn_connect = Button(text='Connect Device', size_hint_y=None, height=80)
+        btn_back = Button(text='Back', size_hint_y=None, height=80)
+
+        btn_connect.bind(on_press=self.go_to_send)
+        btn_back.bind(on_press=partial(self.change_screen, 'main'))
+
+        box1.bind(minimum_height=box1.setter('height'))
+        box1.add_widget(self.lbl)
+        box1.add_widget(self.txt_input_ip)
+        box1.add_widget(btn_connect)
+        box1.add_widget(btn_back)
+
+        anchor = AnchorLayout(anchor_y='top')
+        anchor.add_widget(box1)
+        self.add_widget(anchor)
+
+    def change_screen(self, screen_name, instance):
+        self.manager.current = screen_name
+
+    def go_to_send(self, instance):
+        ip_text = self.txt_input_ip.text
+        if ip_text.count('.') != 3 or len(ip_text) < 10:
+            self.lbl.text = "Error"
+            self.lbl.color = (1, 0, 0, 1)
+        else:
+            self.lbl.text = "IP Address"
+            self.lbl.color = (1, 1, 1, 1)
+            send_screen = self.manager.get_screen('send')
+            send_screen.ip = ip_text
+            self.manager.current = 'send'
+
+
+class SendToDeviceScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ip = ""
+
+        box2 = BoxLayout(orientation='vertical', size_hint=(1, None), spacing=22, padding=[10, 30, 10, 10])
+
+        lbl = Label(text='Your Message', font_size=30)
+        self.txt_input_Message = TextInput(multiline=False, size_hint_y=None, height=50, font_size=30)
+        btn_send = Button(text='Send Message', size_hint_y=None, height=80)
+        btn_back = Button(text='Back', size_hint_y=None, height=80)
+
+        btn_send.bind(on_press=self.send_message)
+        btn_back.bind(on_press=partial(self.change_screen, 'device'))
+
+        box2.bind(minimum_height=box2.setter('height'))
+        box2.add_widget(lbl)
+        box2.add_widget(self.txt_input_Message)
+        box2.add_widget(btn_send)
+        box2.add_widget(btn_back)
+
+        anchor = AnchorLayout(anchor_y='top')
+        anchor.add_widget(box2)
+        self.add_widget(anchor)
+
+    def change_screen(self, screen_name, instance):
+        self.manager.current = screen_name
+
+    def send_message(self, instance):
+        msg = self.txt_input_Message.text
+        try:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect((self.ip, 12345))
+            client.send(msg.encode('utf-8'))
+            response = client.recv(1024).decode('utf-8')
+            print("Response from server:", response)
+            client.close()
+        except Exception as e:
+            print("Connection error:", e)
+
+
+class TimeZoneScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
         box3 = BoxLayout(orientation='vertical', size_hint=(1, None), spacing=22, padding=[10, 30, 10, 10])
 
         self.time_lbl = Label(text=f'Country name Time is 00:00:00', font_size=30)
@@ -39,135 +145,36 @@ class TimeZone(App):
         box3.add_widget(btn_back)
 
         box3.bind(minimum_height=box3.setter('height'))
-        btn_back.bind(on_press=self.go_back)
         btn_find.bind(on_press=self.finder)
-
+        btn_back.bind(on_press=partial(self.change_screen, 'main'))
 
         anchor = AnchorLayout(anchor_y='top')
         anchor.add_widget(box3)
-        return anchor
+        self.add_widget(anchor)
 
-
-    def go_back(self, instance):
-        self.stop()
-        MyApp().run()
+    def change_screen(self, screen_name, instance):
+        self.manager.current = screen_name
 
     def finder(self, instance):
-        if self.txt_input_time.text in country_timezones:
-            tz = ZoneInfo(country_timezones[self.txt_input_time.text])
+        country = self.txt_input_time.text.lower()
+        if country in country_timezones:
+            tz = ZoneInfo(country_timezones[country])
             now = datetime.now(tz)
-            self.time_lbl.color = (1,1,1,1)
-            self.time_lbl.text = f"Country {self.txt_input_time.text.title()} Time is {now.strftime('%H:%M:%S')}"
+            self.time_lbl.color = (1, 1, 1, 1)
+            self.time_lbl.text = f"Country {country.title()} Time is {now.strftime('%H:%M:%S')}"
         else:
             self.time_lbl.text = "Error"
-            self.time_lbl.color = (1,0,0,1)
-        
+            self.time_lbl.color = (1, 0, 0, 1)
 
-class Device(App):
-    def build(self):
-        box1 = BoxLayout(orientation='vertical', size_hint=(1, None), spacing=22, padding=[10, 30, 10, 10])
-
-        self.lbl = Label(text='IP Address', font_size=30)
-        self.txt_input_ip = TextInput(text='192.168.', multiline=False, size_hint_y=None, height=50, font_size=30)
-        btn_connect = Button(text='Connect Device', size_hint_y=None, height=80)
-        btn_back = Button(text='Back', size_hint_y=None, height=80)
-
-        btn_connect.bind(on_press=self.go_to_send)
-        btn_back.bind(on_press=self.go_back)
-
-        box1.bind(minimum_height=box1.setter('height'))
-
-        box1.add_widget(self.lbl)
-        box1.add_widget(self.txt_input_ip)
-        box1.add_widget(btn_connect)
-        box1.add_widget(btn_back)
-
-        anchor = AnchorLayout(anchor_y='top')
-        anchor.add_widget(box1)
-        return anchor
-
-    def go_to_send(self, instance):
-        ip_text = str(self.txt_input_ip.text)
-        if ip_text.count('.') != 3 or len(ip_text) < 10:
-            self.lbl.text = "Error"
-            self.lbl.color = (1,0,0,1)
-        else:
-            self.lbl.text = "IP Address"
-            self.lbl.color = (1,1,1,1)
-            ip = ip_text
-            self.stop()
-            SendToDevice(ip).run()
-
-    def go_back(self, instance):
-        self.stop()
-        MyApp().run()
-
-class SendToDevice(App):
-    def __init__(self, ip, **kwargs):
-        self.ip = ip
-        super().__init__(**kwargs)
-
-    def build(self):
-        box2 = BoxLayout(orientation='vertical', size_hint=(1, None), spacing=22, padding=[10, 30, 10, 10])
-
-        lbl = Label(text='Your Message', font_size=30)
-        self.txt_input_Message = TextInput(multiline=False, size_hint_y=None, height=50, font_size=30)
-        btn_send = Button(text='Send Message', size_hint_y=None, height=80)
-        btn_back = Button(text='Back', size_hint_y=None, height=80)
-
-        btn_send.bind(on_press=self.send_message)
-        btn_back.bind(on_press=self.go_back)
-
-        box2.bind(minimum_height=box2.setter('height'))
-
-        box2.add_widget(lbl)
-        box2.add_widget(self.txt_input_Message)
-        box2.add_widget(btn_send)
-        box2.add_widget(btn_back)
-
-        anchor = AnchorLayout(anchor_y='top')
-        anchor.add_widget(box2)
-        return anchor
-
-    def send_message(self, instance):
-        msg = self.txt_input_Message.text
-        try:
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client.connect((self.ip, 12345))  # پورت دلخواه
-            client.send(msg.encode('utf-8'))
-            response = client.recv(1024).decode('utf-8')
-            print("Response from server:", response)
-            client.close()
-        except Exception as e:
-            print("Connection error:", e)
-
-    def go_back(self, instance):
-        self.stop()
-        Device().run()
 
 class MyApp(App):
     def build(self):
-        box = BoxLayout(orientation='vertical', size_hint=(1, None), spacing=20, padding=10)
-        btn_connect = Button(text='Connect To Device', size_hint_y=None, height=80)
-        btn_time_zone_finder = Button(text="Time Zone Finder", size_hint_y=None, height=80)
-        
-        box.add_widget(btn_connect)
-        box.add_widget(btn_time_zone_finder)
+        sm = ScreenManager()
+        sm.add_widget(MainScreen(name='main'))
+        sm.add_widget(DeviceScreen(name='device'))
+        sm.add_widget(SendToDeviceScreen(name='send'))
+        sm.add_widget(TimeZoneScreen(name='timezone'))
+        return sm
 
-        btn_connect.bind(on_press=self.go_next)
-        btn_time_zone_finder.bind(on_press=self.time_zone)
-        box.bind(minimum_height=box.setter('height'))
-
-        anchor = AnchorLayout(anchor_y='top')
-        anchor.add_widget(box)
-        return anchor
-
-    def go_next(self, instance):
-        self.stop()
-        Device().run()
-
-    def time_zone(self, instance):
-        self.stop()
-        TimeZone().run()
 
 MyApp().run()
